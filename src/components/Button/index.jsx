8b38503ee13ac14f907e06a7405583ac7c70db18
Fransuelton <elton6103@gmail.com>
2023-11-08 14:20:59 -0300

    adjust: adds link to view CV on drive

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -5,19 +5,24 @@ import PropTypes from "prop-types";
 
 const Button = ({ label }) => {
   return (
-    <StyledButton>
+    <StyledButton
+      href="https://drive.google.com/file/d/1WDVqY-55Wc1rAFuuAMqoz7sPUMu3kYdy/view"
+      target="_blank"
+    >
       <StyledIcon icon={faFileArrowDown} />
       {label}
     </StyledButton>
   );
 };
 
-const StyledButton = styled.button`
+const StyledButton = styled.a`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   cursor: pointer;
   width: 20rem;
   height: 6rem;
   padding: 1.6rem 3.2rem;
-  margin-top: 2rem;
   border-radius: 8px;
   background-color: var(--secondary-color);
   font-size: 1.5rem;
@@ -26,6 +31,7 @@ const StyledButton = styled.button`
   transition: 0.5s ease-in-out;
   transition-property: box-shadow;
   border: 1px solid var(--tertiary-color);
+  text-decoration: none;
 
   &:hover {
     box-shadow: 0 0 5px var(--tertiary-color), 0 0 25px var(--tertiary-color),
